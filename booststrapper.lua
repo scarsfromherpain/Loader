4af333eb1527b09c99b60c0ddc29675165c90c1d
scarsfromherpain <ashtonhamilton0192@gmail.com>
2024-07-07 14:40:06 +0100

    Update booststrapper.lua

--- booststrapper.lua
+++ booststrapper.lua
@@ -1,8 +1,6 @@
 
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Consistt/Ui/main/UnLeaked"))()
 
-if game.PlaceId == 13772394625 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/scarsfromherpain/BladeBall/main/loader.lua",true))()
 elseif game.PlaceId == 4566572536 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/scarsfromherpain/Vehicle-Legends/main/main.lua",true))()
 elseif game.PlaceId == 7305309231 then
