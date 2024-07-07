af34f1f0acf134164687dcef443de2f656a32a66
scarsfromherpain <ashtonhamilton0192@gmail.com>
2024-07-07 12:23:49 +0100

    Update booststrapper.lua

--- booststrapper.lua
+++ booststrapper.lua
@@ -2,10 +2,10 @@ local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Consistt/
 
 if game.PlaceId == 13772394625 then
     loadstring(game:HttpGet("https://github.com/scarsfromherpain/BladeBall/blob/main/loader.lua",true))()
-elseif game.PlaceId == 301549746 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/scarsfromherpain/CounterBlox/main/main.lua",true))()
 elseif game.PlaceId == 4566572536 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/scarsfromherpain/Vehicle-Legends/main/main.lua",true))()
+elseif game.PlaceId == 301549746 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scarsfromherpain/CounterBlox/main/main.lua",true))()
 elseif game.PlaceId == 7305309231 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/scarsfromherpain/taxi-boss/main/main.lua",true))()
 else
