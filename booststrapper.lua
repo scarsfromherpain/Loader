a3f9335b74807c4343b0ce37a266ab64af64acd7
scarsfromherpain <ashtonhamilton0192@gmail.com>
2024-07-07 11:42:47 +0100

    Create booststrapper.lua

---
+++ booststrapper.lua
@@ -0,0 +1,11 @@
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Consistt/Ui/main/UnLeaked"))()
+
+if game.PlaceId == 13772394625 then
+    loadstring(game:HttpGet("https://github.com/scarsfromherpain/BladeBall/blob/main/loader.lua",true))()
+elseif game.PlaceId == 301549746 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scarsfromherpain/CounterBlox/main/main.lua",true))()
+elseif game.PlaceId == 4566572536 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scarsfromherpain/Vehicle-Legends/main/main.lua",true))()
+else
+    local notsupported = lib:InitNotifications():Notify("This game isn't supported for Skeet yet. If this game is popular please add in suggestions", 3, "information")
+end
