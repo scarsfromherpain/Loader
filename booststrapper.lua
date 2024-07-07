a436ff1cd73694ac82d93bbfbac3823a2a113fa5
scarsfromherpain <ashtonhamilton0192@gmail.com>
2024-07-07 11:55:21 +0100

    Update booststrapper.lua

--- booststrapper.lua
+++ booststrapper.lua
@@ -6,6 +6,9 @@ elseif game.PlaceId == 301549746 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/scarsfromherpain/CounterBlox/main/main.lua",true))()
 elseif game.PlaceId == 4566572536 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/scarsfromherpain/Vehicle-Legends/main/main.lua",true))()
+elseif game.PlaceId == 7305309231 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scarsfromherpain/taxi-boss/main/main.lua",true))()
 else
     local notsupported = lib:InitNotifications():Notify("This game isn't supported for Skeet yet. If this game is popular please add in suggestions", 3, "information")
 end
+7305309231
